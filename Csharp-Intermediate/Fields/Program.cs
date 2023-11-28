40dbd886d71789d7b4a2ef49fe6cac14fda0250a
iamurtazo <murtazo207@inha.edu>
2023-11-28 18:59:30 +0500

    indexers as separate class

--- Csharp-Intermediate/Fields/Program.cs
+++ Csharp-Intermediate/Fields/Program.cs
@@ -1,30 +1,16 @@
-﻿public class HttpCookie
-{
-    private readonly Dictionary<string, string> _dictionary = new Dictionary<string, string>();
-
-    public string this[string key]
-    {
-        get
-        {
-            return _dictionary[key];
-        }
-        set
-        {
-            _dictionary[key] = value;
-        }
-
-    }
-}
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        
+        //UseIndexers();
+        //UseProperty();
+        //UseCase();
+    }
+    static void UseIndexers()
+    {
         var httpCookie = new HttpCookie();
         httpCookie["name"] = "Mosh";
         Console.WriteLine(httpCookie["name"]);
-        
-
     }
     static void UseProperty()
     {
