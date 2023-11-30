e332fb0e04e9ebac09034c3761fb549764534653
iamurtazo <murtazo207@inha.edu>
2023-11-30 23:49:35 +0500

    Constructors and Inheritance

--- Csharp-Intermediate/Inheritance/Program.cs
+++ Csharp-Intermediate/Inheritance/Program.cs
@@ -1,19 +1,40 @@
 ﻿//namespace Csharp-Intermediate
 using System;
-
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Amazon.CalculateRating();
-        }
-        /* static void UseProtected()
-        {
-            // access modifiers
-            var customer = new Customer();
-            customer.Name = "Uncle Bob";
-            customer.Id = 3;
-            customer.Promote();
-        } */
+        var car = new Car("D 654 XA");
+
+        //Amazon.CalculateRating();
     }
+    /* static void UseProtected()
+    {
+        // access modifiers
+        var customer = new Customer();
+        customer.Name = "Uncle Bob";
+        customer.Id = 3;
+        customer.Promote();
+    } */
+}
+public class Vehicle 
+{
+    private string _registrationNumber;
+    /* public Vehicle()
+    {
+    } */
+    public Vehicle (string registrationNumber)
+    {
+        this._registrationNumber = registrationNumber;
+        Console.WriteLine("Vehicle is initialized {0}", registrationNumber); 
+    }
+}
+
+public class Car : Vehicle
+{
+    public Car(string registrationNumber) : base(registrationNumber)
+    {
+        Console.WriteLine("Car is initialized {0}", registrationNumber);
+    }
+}
     
