e1d0c0ab918e3f7db54bffb3384b29fc8af27047
iamurtazo <murtazo207@inha.edu>
2023-11-30 20:10:08 +0500

    protected & private example

---
+++ Csharp-Intermediate/Inheritance/Customer.cs
@@ -0,0 +1,20 @@
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public void Promote()
+    {
+        var rating = CalculateRating(excludeOrders : true);
+        if(rating == 0)
+            Console.WriteLine("Promoted to level 1");
+        else
+            Console.WriteLine("Promoted to level 2");
+    }
+    private int CalculateRating(bool excludeOrders)
+    {
+        return 1;
+    }
+}
+
+
--- Csharp-Intermediate/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ Csharp-Intermediate/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1dabb108194619316df4483e5fcb349d37ffc76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
