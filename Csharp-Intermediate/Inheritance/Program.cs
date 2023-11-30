3642617fcce4c655429a877c3e663da4aab98591
iamurtazo <murtazo207@inha.edu>
2023-11-30 23:58:06 +0500

    separate class

--- Csharp-Intermediate/Inheritance/Program.cs
+++ Csharp-Intermediate/Inheritance/Program.cs
@@ -20,21 +20,4 @@ class Program
         customer.Promote();
     } */
 }
-public class Shape
-{
-    public int Width { get; set; }
-    public int Height { get; set; }
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    public void Draw()
-    {
-        
-    }
-}
-
-public class Text : Shape{
-    public int FontSize { get; set; }
-    public string FontName { get; set; }
-}
     
---
+++ Csharp-Intermediate/Inheritance/Text.cs
@@ -0,0 +1,6 @@
+//namespace Csharp-Intermediate
+public class Text : Shape{
+    public int FontSize { get; set; }
+    public string FontName { get; set; }
+}
+    
