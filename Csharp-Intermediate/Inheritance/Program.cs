a959dbdc4bd18c40ed0bc2ac44c13f5573122f13
iamurtazo <murtazo207@inha.edu>
2023-12-01 02:59:51 +0500

    Upcasting and Downcasting

--- Csharp-Intermediate/Inheritance/Program.cs
+++ Csharp-Intermediate/Inheritance/Program.cs
@@ -1,12 +1,42 @@
 ﻿//namespace Csharp-Intermediate
 using System;
+using System.IO;
+enum Days
+{
+    Sunday,
+    Monday,
+    Tuesday, 
+    Wednesday,
+    Thursday,
+    Friday, 
+    Saturday
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Text text = new Text();
+
+        Days today = Days.Wednesday;
+
+        var todayValue = (int)today;
+        Console.WriteLine($"todayValue: {todayValue}, today: {today}");
+    
+        /* Shape shape = new Text();
+        Text text = (Text) shape; */
+        /* Text text = new Text();
         Shape shape = text;
-        Console.WriteLine(shape.GetType());
+
+        var birsonda = 56;
+        //var path = @"C:/Users/iammu/c_problems/main.c";
+        StreamReader reader = new StreamReader(new MemoryStream(birsonda));
+        Console.WriteLine("reader: " + reader); */
+
+        // places we use this kind of converson
+        // * 
+
+    
+        //Console.WriteLine(shape.GetType());
         
         //var car = new Car("D 654 XA");
         //Amazon.CalculateRating();
--- Csharp-Intermediate/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ Csharp-Intermediate/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3642617fcce4c655429a877c3e663da4aab98591")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
