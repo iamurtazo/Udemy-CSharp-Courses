54d49d44e03bbdc445887d85ddcc1efb85767355
iamurtazo <murtazo207@inha.edu>
2023-11-30 23:57:22 +0500

    Upcasting and Downcasting

--- Csharp-Intermediate/Inheritance/Program.cs
+++ Csharp-Intermediate/Inheritance/Program.cs
@@ -4,8 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        var car = new Car("D 654 XA");
-
+        Text text = new Text();
+        Shape shape = text;
+        Console.WriteLine(shape.GetType());
+        
+        //var car = new Car("D 654 XA");
         //Amazon.CalculateRating();
     }
     /* static void UseProtected()
@@ -16,5 +19,22 @@ class Program
         customer.Id = 3;
         customer.Promote();
     } */
+}
+public class Shape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public void Draw()
+    {
+        
+    }
+}
+
+public class Text : Shape{
+    public int FontSize { get; set; }
+    public string FontName { get; set; }
 }
     
--- Csharp-Intermediate/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
+++ Csharp-Intermediate/Inheritance/obj/Debug/net8.0/Inheritance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e5cf19673654438c5b4d3c254a672bf444ff5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
