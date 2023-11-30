e1dabb108194619316df4483e5fcb349d37ffc76
iamurtazo <murtazo207@inha.edu>
2023-11-30 19:29:31 +0500

    access modifiers

---
+++ Csharp-Intermediate/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // access modifiers
+        var customer = new Customer();
+        
+
+    }
+}
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public void Promote()
+    {
+
+    }
+}
+
+
