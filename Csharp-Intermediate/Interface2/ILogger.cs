e7e71d2b5c7427522e6abfe4c2d72e30620169d4
iamurtazo <murtazo207@inha.edu>
2023-12-06 14:19:01 +0500

    some changes

---
+++ Csharp-Intermediate/Interface2/ILogger.cs
@@ -0,0 +1,8 @@
+// See https://aka.ms/new-console-template for more information
+
+public interface ILogger
+{
+    void LogInfo(string message);
+    void LogError(string message);
+
+}
--- Csharp-Intermediate/Interface2/Program.cs
+++ Csharp-Intermediate/Interface2/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dbMigrator = new DbMigrator();
+        
+    }
+}
+
+public class DbMigrator : ILogger
+{
+    private readonly ILogger _logger;
+
+    public DbMigrator(ILogger logger)
+    {
+        this._logger = logger;
+    }
+
+    public void Migrate()
+    {
+        _logger.LogInfo("Migration started at {0}" + DateTime.Now);
+        _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
+       
+    }
+
+
+    public void LogError(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+}
