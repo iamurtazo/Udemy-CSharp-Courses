ca28784c950e94c00c8bb02af291f12d03f26b27
iamurtazo <murtazo207@inha.edu>
2023-12-11 20:05:25 +0500

    generics and functionalities

--- Csharp-Advanced/Generics/Program.cs
+++ Csharp-Advanced/Generics/Program.cs
@@ -1,7 +1,13 @@
-﻿class Program
+﻿using System.Collections;
+
+partial class Program
 {
     static void Main(string[] args)
     {
-        
+        // Generics
+        var digits = new GenericList<string>();
+        digits.Add("C# Advanced");
+        digits.Add(new string("Alabama"));
+
     }
 }
