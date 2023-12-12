afe3b0ee7a8b6831e27d2e53bce22d3d4d59ef20
iamurtazo <murtazo207@inha.edu>
2023-12-12 16:50:44 +0500

    separate classes

---
+++ Csharp-Advanced/Delegates/PhotoFilters.cs
@@ -0,0 +1,17 @@
+public class PhotoFilters
+{
+    public void Resize(Photo photo)
+    {
+        Console.WriteLine("Resize photo");
+    }
+
+    public void ApplyContrast(Photo photo)
+    {
+        Console.WriteLine("Applying contrast");
+    }
+
+    public void ApplyBrightness(Photo photo)
+    {
+        Console.WriteLine("Applying brightness");
+    }
+}
--- Csharp-Advanced/Delegates/Program.cs
+++ Csharp-Advanced/Delegates/Program.cs
@@ -1,59 +1,9 @@
-﻿using Microsoft.VisualBasic;
-
-public class Program
+﻿public class Program
 {
     static void Main(string[] args)
     {
-
-    }
-}
-public class PhotoProcessor
-{
-    public delegate void PhotoFilterHandler(Photo photo);
-
-    public void Process(string path)
-    {
-        var photo = new Photo.Load(path);
-
-        var filters = new PhotoFilters();
-        filters = ApplyBrightness(photo);
-        filters = ApplyContrast(photo);
-        filters = Resize(photo);
-
-        photo.Save();
-    }
-
-}
-
-internal class PhotoFilters
-{
-    public PhotoFilters()
-    {
-    }
-    public void Resize()
-    {
-        Console.WriteLine() 
-    }
-
-    public void ApplyContrast()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void ApplyBrightness(Photo.Load photo)
-    {
-        throw new NotImplementedException();
-    }
-}
-
-public class Photo
-{
-    public static Photo Load(string path)
-    {
-        return new Photo();
-    }
-    public void Save()
-    {
-
+        var processor = new PhotoProcessor();
+        processor.Process("photo.jpg");
+        
     }
 }
