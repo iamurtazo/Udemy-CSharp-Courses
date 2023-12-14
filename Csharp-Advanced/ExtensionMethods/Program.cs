8224619e96ae78b06bc8c549eb9cb8c0f487db5e
iamurtazo <murtazo207@inha.edu>
2023-12-14 20:18:31 +0500

    ...

--- Csharp-Advanced/ExtensionMethods/Program.cs
+++ Csharp-Advanced/ExtensionMethods/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string post = "This is a very long post that gonna include only a few words)) haha joke";
+        Console.WriteLine(post.Shorten(5));
+
+    }   
+}
+public static class StringExtension
+{
+    public static string Shorten(this String text, int num)
+    {
+        if (num < 0)
+            throw new InvalidOperationException("Number should be greater than 0;");
+        if (num == 0)
+            return "";
+        var textSplitted = text.Split(" ");
+
+        return string.Join(" ", textSplitted.Take(num));
+    }
+}
